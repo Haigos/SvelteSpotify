ab78b878e889c2c0e7e54701f4a403a598e8baf8
Haigos <timzurb@gmail.com>
2023-03-16 09:52:16 +0100

    Authentication

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import 'modern-normalize/modern-normalize.css';
+	import '../styles/main.scss';
+	import type { LayoutData } from './$types';
+
+	export let data: LayoutData;
+
+	$: user = data.user;
+</script>
+
+{#if user}
+	<p>Hello {user.display_name}</p>
+	<form method="POST" action="/api/auth/logout">
+		<button type="submit">Logout</button>
+	</form>
+{/if}
+
+<slot />
